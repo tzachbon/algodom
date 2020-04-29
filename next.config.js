880212848b28a04e72c0e8a8596b51c2bcb6cdc5
tzachbon <tzachbonfil@gmail.com>
2020-04-29 16:00:04 +0300

    .

--- next.config.js
+++ next.config.js
@@ -1,9 +1,4 @@
-const withSass = require('@zeit/next-sass');
-const isProd = (process.env.NODE_ENV || 'production') === 'production';
 
-module.exports = withSass({
-  exportPathMap: () => ({
-    '/': { page: '/' },
-  }),
-  assetPrefix: isProd ? '/algodom' : '',
-});
+
+const withSass = require('@zeit/next-sass');
+module.exports = withSass({});
